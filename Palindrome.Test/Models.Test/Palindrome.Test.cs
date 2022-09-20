7873d957c53adb2f3cd6c1416b62fb2f54ebe6b9
tgold83 <ted.jonathan.gold@gmail.com>
2022-09-20 15:03:28 -0700

    add green test for IsPalindrome Test
    Co-Authored-By: Noah Lundquist <nalundquist@gmail.com>

--- Palindrome.Test/Models.Test/Palindrome.Test.cs
+++ Palindrome.Test/Models.Test/Palindrome.Test.cs
@@ -10,10 +10,7 @@ namespace Program.Pal.Test
     public void Palindrome_IsPalindrome_True()
     {
       Palindrome pal = new Palindrome("racecar");
-			Assert.AreEqual(true, pal.IsPalindrome());
-			// split string into array
-			// reverse array
-			// check if array == reverseArray; if true, palindrome, if false, nah
+			Assert.AreEqual(true, pal.IsPalindrome(pal.Palin));
     }
 	}
 }
